df6b6e2d1debf77503963746b520eb10eac572ad
mkinet <mkinet@gmail.com>
2015-10-27 16:47:23 +0100

    Implementation of Gaspec input file creation

--- PrePostGaspec.py
+++ PrePostGaspec.py
@@ -4,7 +4,7 @@ from SeismicTools import *
 gaspec='C:/Programs/Gaspec/gaspec.exe'
 
 class Analysis():
-    def __init__(self,pwd=None,name=None,case=None):        
+    def __init__(self,pwd=None,name=None,case=None,THinputlist=[]):
         #Get working directory : Working directory is set to be the
         #directory where the script is executed
         if pwd==None:
@@ -19,14 +19,14 @@ class Analysis():
             case='CAS'
         self.case=case
         ## Input TH, or family of TH??
-        self.accelerogram = TimeHistoryGaspec()
+        self.accelerograms = TimeHistoryFamily(family=THinputlist)
         # Initialize results. We mainly focus on the set of spectra
         # generated
         self.spectralistout=SpectraFamilyGaspec()
         # Initialize parser. THIS IS USED FOR PPROC.
         #self.parser=FileParser(self.datafiles)
     
-    def GenInfile(self,fadd=[]):
+    def GenInfile(self,fadd=[],amo=[]):
         # 1) input file containing analysis name and case name.
         f=open('gaspec.dat','w')
         f.write(self.name+'\n'+self.case+'\n')
@@ -38,19 +38,100 @@ class Analysis():
         # Commande START
         f.write('START\n'+self.name+'\n'+self.case+'\n'\
                 +'UNIT G\n')
-        # Commande FREQUENCE. we set IADD to 0 by default : all
+        # Commande FREQUENCE. We set IADD to 0 by default : all
         # previously computed frequencies are kept and we add the
         # frequencies in list fadd
         if len(fadd)>0:
             f.write('FREQUENCES'+9*' '+'0'+5*' '+str(len(fadd))+'\n')
             count=0
             for freq in fadd:
-                f.write(6*' '+str.format("{0:.2f}", freq))        
+                f.write(str.format("{:10.2f}", freq))        
                 count+=1
                 if count==7:
                     f.write('\n')
                     count=0
                     
+        # Commande AMORTISSEMENTS.
+        if len(amo)>0:
+            f.write('AMORTISSEMENTS'+4*' '+str(len(amo))+'\n')
+            count=0
+            for a in amo:
+                f.write(str.format("{:10.2f}", a))        
+                count+=1
+                if count==7:
+                    f.write('\n')
+                    count=0
+            if count!=0:f.write('\n')
+        # Commande ACCELEROGRAMMES/INPUT.
+        f.write('ACCELEROGRAMMES\n')
+        # We use default values for IPU and IPL
+        # IPU = 0 means there is no output of accelerograms made by
+        # GASPEC. IPL = 0 means there is no figure made by GASPEC.
+        f.write(str.format("{:15s}",'INPUT')+str.format("{:5d}",0)\
+                +'\n')
+        # This is a multiplicative factor. We hardcode it to 1.
+        f.write(' 1.\n')
+        # Now we need to write the values of the accelerograms :
+        for accel in self.accelerograms:
+            f.write(#str.format("{:5s}",accel.name)+\
+                    str.format("{:5s}","E999")+\
+                    str.format("{:5d}",1)+\
+                    str.format("{:5d}",1)+\
+                    str.format("{:5d}",accel.npoints)+\
+                    str.format("{:10.3f}",accel.dt)\
+                    +5*' '+'(7F10.2)\n')
+            count=0
+            for val in accel.accel:
+                f.write(str.format("{:10.2f}", val))        
+                count+=1
+                if count==7:
+                    f.write('\n')
+                    count=0
+            if count!=0:f.write('\n')
+        f.write('END\n')
+        # # Commande ACCELEROGRAMMES/OUTPUT.
+        # # carte 1
+        # f.write('ACCELEROGRAMMES\n')
+        # # We use default values for IOR, IPU and IPL.
+        # # IPU = 1 means output of accelerograms to 80 columns
+        # # formatted file. IPL = 0 means there is no figure made by
+        # # GASPEC.
+        # # carte 2
+        # f.write('OUTPUT')
+        # # carte 3
+        # f.write(str.format("{:5d}",0)+
+        #         str.format("{:5d}",1)+
+        #         str.format("{:5d}",0)+'\n')
+        # # carte 4 et suivantes
+        # 
+        # # carte 5
+        # f.write('END\n')
+
+
+        # Commande SPECTRES
+        # carte 1
+        f.write(str.format("{:15s}","SPECTRES")+
+                str.format("{:5d}",0)+
+                str.format("{:5d}",-1)+
+                str.format("{:5d}",0)+
+                str.format("{:5d}",0)+'\n')
+        # carte 2
+        f.write(str.format("{:10.1f}",1.0)+
+                str.format("{:20s}",' unites')+
+                str.format("{:10.0F}",100.0)+'\n')
+        # carte 3 et suivantes
+        for accel in self.accelerograms:
+            f.write(#str.format("{:5s}",accel.name)+\
+                    str.format("{:5s}","E999")+\
+                    str.format("{:5d}",1)+\
+                    str.format("{:5d}",1)+\
+                    str.format("{:5d}",0)+'\n')
+        # derniere carte
+        f.write('END\n')
+
+        # Commande CHECK
+        f.write('CHECK')
+        
     def RunAnalysis(self):
         errfile=os.path.join(self.pwd,'gaspec.err')
         err=open(errfile,'w')
@@ -264,9 +345,10 @@ class SpectraFamilyGaspec(SpectraFamily):
         
     
 def Main():
-    A1=Analysis(name='TEST',case='CAS')
-    A1.GenInfile(fadd=[0.10,0.119,0.1412563,0.10,0.119,0.1412563,0.10,0.119,0.1412563,0.10,0.119,0.1412563])
-    #A1.RunAnalysis()
+    TH1=TimeHistory(accel=[0.0,0.1,0.2,0.3,0.2,0.1,0.0,-0.1,-0.2,-0.2,-0.1,0.0,0.6],dt=0.1)
+    A1=Analysis(name='TEST',case='CAS',THinputlist=[TH1])
+    A1.GenInfile(amo=[0.02])
+    A1.RunAnalysis()
             
 if __name__=="__main__":
     Main()
